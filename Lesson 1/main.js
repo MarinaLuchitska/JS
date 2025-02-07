c63b11f361ecc0193143727047fe04764d6a8558
MarinaLuchitska <lmarina3891@gmail.com>
2025-02-07 21:55:49 +0200

    Initial commit

---
+++ Lesson 1/main.js
@@ -0,0 +1,58 @@
+
+// #dYQNrBV
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+//
+// #6Qb97gsv
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+//
+// #4N0y5tufA
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+// ----------------------------------#1------------------------------------
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 1;
+let e = 10;
+let f = -999;
+let g = 123;
+let h = 3.14;
+let i = 2.7;
+let j = 16;
+let k = true;
+let l = false
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+// -----------------------------------#2-------------------------------
+let firstName = 'Luchitska';
+let middleName = 'Marina';
+let lastName = 'Oleksandrivna';
+
+let fullName = firstName + ' ' + middleName + ' ' + lastName;
+console.log(fullName)
+// -----------------------------#3------------------------------
+let A = 100;
+let B = '100';
+let C = true;
+
+console.log(typeof A,A);
+console.log(typeof B,B);
+console.log(typeof C,C);
+
+
+
+
+
+
